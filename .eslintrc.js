8be25aaec31d41016cf4e14e6bf3069a69afba73
Henry Franceschi <69153058+henryfranceschi@users.noreply.github.com>
2021-10-04 18:14:36 +0200

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,8 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking"
     ],
-    ignorePatterns: ['.eslintrc.js']
+    ignorePatterns: [
+        '.eslintrc.js',
+        'dist'
+    ]
 };
